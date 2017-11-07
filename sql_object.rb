6a21a4e2676778a8523e60b09885571b57e057b5
Kevin Coppa <khc339@nyu.edu>
2017-11-07 15:31:51 -0500

    Write insert and update functionality to SQLObject

--- sql_object.rb
+++ sql_object.rb
@@ -22,6 +22,90 @@ class SQLObject
     @table_name ||= self.name.tableize
   end
 
-  
+  def self.all
+    data = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+    parse_all(data)
+  end
 
+  def self.parse_all(data)
+    data.map{ |datum| self.new(datum) }
+  end
+
+  def self.find(id)
+    data = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+    parse_all(data)[0]
+  end
+
+  def self.columns
+    return @columns if @columns
+    column_names = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    column_names.map!(&:to_sym)
+    @columns = column_names
+  end
+
+  def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    self.class.columns.map{ |attribute| self.send(attribute) }
+  end
+
+  def insert
+    col_strings = self.class.columns.drop(1)
+    columns = col_strings.map(&:to_s).join(", ")
+    question_marks = (["?"] * col_strings.count).join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{columns})
+      VALUES
+        (#{question_marks})
+    SQL
+    self.id = DBConnection.last_insert_row_id
+  end
+
+  def update
+    set_line = self.class.columns.map{ |attribute| "#{attribute} = ?"}.join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def save
+    id.nil? ? insert : update
+  end
 end
