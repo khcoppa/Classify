9df9fd79141715794602afca037fcaff719a4cfd
Kevin Coppa <khc339@nyu.edu>
2017-11-07 15:45:36 -0500

    Write initialize methods for HasManyOptions and BelongsToOptions

--- associatable.rb
+++ associatable.rb
@@ -16,3 +16,29 @@ class AssocOptions
     model_class.table_name
   end
 end
+
+class BelongsToOptions < AssocOptions
+  def initialize(name, options = {})
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :primary_key => :id,
+      :class_name => name.to_s.camelcase
+    }
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+  end
+end
+
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :primary_key => :id,
+      :class_name => name.to_s.camelcase.singularize
+    }
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+  end
+end
